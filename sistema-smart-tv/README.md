d97c484a3a833b157e04ce5769f5c06619436c0e
crggoes <crgg@onionmail.org>
2022-10-20 10:11:01 -0300

    Sistema-SmarTV

---
+++ sistema-smart-tv/README.md
@@ -0,0 +1,4 @@
+## Sistema-SmarTV
+
+Sistema simples efetuado em Java com a instrução do Gleyson Sampaio - Curso Básico da Dio Java Beginners.
+
---
+++ tipos-variáveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+       int numero 1 = 1;
+       int numero 2 = 2;
+
+       if(numero1 > numero 2)
+       
+       
+       System.out.println(resultado);
+
+
+    }
+
+
+}
